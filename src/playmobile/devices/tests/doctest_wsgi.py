b0667d3c42e5dd2fbb04274621f5e8ab693a1f31
Antonin Amand <antonin.amand@gmail.com>
2010-03-23 16:45:02 +0100

    add tests for cookies

--- src/playmobile/devices/tests/doctest_wsgi.py
+++ src/playmobile/devices/tests/doctest_wsgi.py
@@ -8,9 +8,11 @@
 
     The environment is set with an user agent string.
 
-    >>> environ = {'HTTP_USER_AGENT': "Mozilla/5.0 (SymbianOS/9.1; U; [en]; Series60/3.0 NokiaE60/4.06.0) AppleWebKit/413 (KHTML, like Gecko) Safari/413"}
-    >>> wrapped(environ, lambda *args: None)
-    ['hello!']
+    >>> from webob import Request
+    >>> request = Request.blank('/')
+    >>> request.environ['HTTP_USER_AGENT'] = "Mozilla/5.0 (SymbianOS/9.1; U; [en]; Series60/3.0 NokiaE60/4.06.0) AppleWebKit/413 (KHTML, like Gecko) Safari/413"
+    >>> request.call_application(wrapped) #doctest: +ELLIPSIS
+    ('200 Ok..., ['hello!'])
 
     Now we can check that the environment contains some info about the device.
 
@@ -19,9 +21,30 @@
     >>> app.environ['playmobile.devices.marker']
     <InterfaceClass playmobile.interfaces.devices.IBasicDeviceType>
 
-    >>> environ = {'HTTP_USER_AGENT': "Mozilla/5.0 (iPhone Simulator; U; CPU iPhone OS 3_1_3 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7E18 Safari/528.16"}
-    >>> wrapped(environ, lambda *args: None)
-    ['hello!']
+    >>> request = Request.blank('/')
+    >>> request.environ['HTTP_USER_AGENT'] = "Mozilla/5.0 (iPhone Simulator; U; CPU iPhone OS 3_1_3 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7E18 Safari/528.16"
+    >>> request.call_application(wrapped) #doctest: +ELLIPSIS
+    ('200 Ok..., ['hello!'])
+    >>> app.environ['playmobile.devices.marker_name']
+    'IAdvancedDeviceType'
+    >>> app.environ['playmobile.devices.marker']
+    <InterfaceClass playmobile.interfaces.devices.IAdvancedDeviceType>
+
+    When the response returns the middle sets a cookie to cache the result
+    on the client side.
+
+    >>> request = Request.blank('/')
+    >>> response = request.get_response(wrapped)
+    >>> response.headers['Set-Cookie'] # doctest: +ELLIPSIS
+    '__dt=basic;...'
+
+    When client sends a cookie it is used as a cache.
+
+    >>> request = Request.blank('/')
+    >>> request.environ['HTTP_COOKIE'] = '__dt=advanced'
+    >>> response = request.get_response(wrapped)
+    >>> response.headers.get('Set-Cookie', None) is None
+    True
     >>> app.environ['playmobile.devices.marker_name']
     'IAdvancedDeviceType'
     >>> app.environ['playmobile.devices.marker']
@@ -34,6 +57,7 @@ class TestApp(object):
     """ it only sets the environ attribute. So we can verify some expectations
     on it.
     """
+    environ = {}
 
     def __call__(self, environ, start_response):
         self.environ = environ
